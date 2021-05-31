c8b63a45eb5a0c62721a821dddb0d16bb1d5e674
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2021-05-31 12:43:12 -0400

    make package

--- R/cleaning_functions.R
+++ R/cleaning_functions.R
@@ -1,69 +1,6 @@
-library(tidyverse)
 
-
-# nflfastr pbp 2017-2020
-pbp <- readRDS("data-raw/nflfastr_plays.rds")
-
-lee <- nflfastR:::load_lees_games() %>%
-  # since no tracking data before 2017
-  filter(season >= 2017)
-
-schedule <- lee %>%
-  select(season, game_id = old_game_id) %>%
-  mutate(game_id = as.integer(game_id))
-
-max_year <- lee %>%
-  filter(!is.na(result), season >= 2020) %>%
-  tail(1) %>%
-  pull(season)
-
-load_nflfastr <- function(y) {
-  
-    .url <- glue::glue("https://github.com/nflverse/nflfastR-data/blob/master/data/play_by_play_{y}.rds?raw=true")
-    con <- url(.url)
-    pbp <- readRDS(con)
-    close(con)
-    return(pbp)
-
-}
-
-# get newer pbp if it exists
-if (max_year > 2020) {
-  
-  new_pbp <- map_df(max_year, load_nflfastr) %>%
-    dplyr::rename(nflfastr_game_id = game_id, game_id = old_game_id) %>%
-    dplyr::select(
-      nflfastr_game_id, 
-      game_id, 
-      play_id, 
-      week,
-      posteam, 
-      home_team, 
-      away_team, 
-      down, 
-      ydstogo, 
-      yardline_100,
-      qtr, 
-      epa, 
-      yards_gained, 
-      air_yards, 
-      desc, 
-      pass,
-      rush,
-      play_type_nfl
-    ) %>%
-    dplyr::mutate(game_id = as.integer(game_id))
-  
-  pbp <- bind_rows(pbp, new_pbp)
-  
-}
-
-
-# get team colors and logo for joining 
-colors <- nflfastR::teams_colors_logos %>%
-  select(team_name = team_abbr, team_color, team_color2, team_logo_espn)
-
-wrapper <- function(df) {
+#' @export
+clean_and_rotate <- function(df) {
   df %>%
     add_info() %>%
     rotate_to_ltr() %>%
@@ -72,20 +9,20 @@ wrapper <- function(df) {
 
 # creates team_name, defense, and adds some play info from nflfastr
 add_info <- function(df) {
-  
+
   # make column names look reasonable
   df <- df %>%
     janitor::clean_names()
-  
+
   # NGS highlights use "frame" instead of "frame_id" so make frame_id for these
   if (!"frame_id" %in% names(df) & "frame" %in% names(df)) {
     df <- df %>%
       dplyr::rename(frame_id = frame)
   }
-  
+
   # NGS highlights have home_team_flag instead of team
   if (!"team" %in% names(df)) {
-    
+
     df <- df %>%
       mutate(
         team = case_when(
@@ -94,30 +31,30 @@ add_info <- function(df) {
           is.na(home_team_flag) ~ "football"
         )
       )
-    
+
   }
-  
+
   # 2020 bdb used "orientation" instead of "o"
   if ("orientation" %in% names(df)) {
     df <- df %>%
       dplyr::rename(o = orientation)
   }
-  
+
   # 2020 big data bowl has weird play IDs where game_id is pre-pended
   if (max(nchar(df$play_id)) > 10) {
-    
+
     df <- df %>%
       mutate(
         play_id = substr(play_id, 11, 14) %>% as.integer(),
         game_id = as.integer(game_id),
-        
+
         # since bdb only has handoffs and doesn't have event, put in the event
         event = "handoff"
       )
-    
+
   }
-  
-  
+
+
   df %>%
     # get rid of the columns we're joining so no join duplicates
     select(-tidyselect::any_of(c(
@@ -140,29 +77,32 @@ add_info <- function(df) {
       )
     ) %>%
     left_join(colors, by = "team_name") %>%
-    mutate(team_color = ifelse(team_name == "football", "#663300", team_color)) %>%
+    mutate(
+      team_color = ifelse(team_name == "football", "#663300", team_color),
+      play = paste0(game_id, "_", play_id)
+      ) %>%
     return()
-  
+
 }
 
 # rotate field so all plays are left to right
 # affects x, y, o, dir
 rotate_to_ltr <- function(df) {
-  
+
   if (!"play_direction" %in% names(df)) {
     message("Can't find play direction. Inferring from offense & defense locations at snap")
-    
-    df <- df %>% 
-      filter(event == "ball_snap", team != "football") %>% 
+
+    df <- df %>%
+      filter(event == "ball_snap", team != "football") %>%
       group_by(game_id, play_id, defense) %>%
       summarize(mean_x = mean(x, na.rm = T)) %>%
       pivot_wider(names_from = defense, values_from = mean_x, names_prefix = "x_") %>%
       ungroup() %>%
       mutate(
-        play_direction = 
+        play_direction =
           ifelse(
             # if defense has bigger x than offense, it's left to right
-            x_1 > x_0, 
+            x_1 > x_0,
             "right",
             "left"
             )
@@ -171,128 +111,130 @@ rotate_to_ltr <- function(df) {
     inner_join(df,  by = c("game_id", "play_id"))
 
   }
-  
+
   # now we're ready to flip everything on left-moving plays
   df <- df %>%
     mutate(
       # standardize all plays so they are left to right
       to_left = ifelse(play_direction == "left", 1, 0),
-      
+
       # reflect x & y
       x = ifelse(to_left == 1, 120 - x, x),
       y = ifelse(to_left == 1, 160/3 - y, y),
-      
+
       # get x value of line of scrimmage
       los_x = 110 - yardline_100,
       dist_from_los = x - los_x
     )
-  
+
   # if orientation is in df, standardize it
   if ("o" %in% names(df)) {
     df <- df %>%
       mutate(
         # rotate 180 degrees for the angles
-        o = ifelse(to_left == 1, o + 180, o), 
-        
+        o = ifelse(to_left == 1, o + 180, o),
+
         # make sure measured 0 to 360
         o = ifelse(o > 360, o - 360, o),
-        
+
         # convert to radians
         o_rad = pi * (o / 180),
-        
+
         # get orientation and direction in x and y direction
         # NA checks are for the ball
         o_x = ifelse(is.na(o), NA_real_, sin(o_rad)),
         o_y = ifelse(is.na(o), NA_real_, cos(o_rad))
       )
   }
-  
+
   # if dir is in df, standardize it
   if ("dir" %in% names(df)) {
     df <- df %>%
       mutate(
         # rotate 180 degrees for the angles
         dir = ifelse(to_left == 1, dir + 180, dir),
-        
+
         # make sure measured 0 to 360
         dir = ifelse(dir > 360, dir - 360, dir),
-        
+
         # convert to radians
         dir_rad = pi * (dir / 180),
-        
+
         # get orientation and direction in x and y direction
         # NA checks are for the ball
         dir_x = ifelse(is.na(dir), NA_real_, sin(dir_rad)),
         dir_y = ifelse(is.na(dir), NA_real_, cos(dir_rad)),
-        
+
         s_x = dir_x * s,
         s_y = dir_y * s,
-        
+
         a_x = dir_x * a,
         a_y = dir_y * a
       )
   }
-  
+
   return(df)
-  
+
 }
 
 
 
-# compute angle difference between x and y and 
+# compute angle difference between x and y and
 # some prefix_x and prefix_y,
 # returning o_to_prefix
 
 # TODO: deal with multiple QB in one play
+#' @export
 compute_o_diff <- function(df, prefix = "qb") {
-  
+
   name_x <- sym(paste0(prefix, "_x"))
   name_y <- sym(paste0(prefix, "_y"))
-  
+
   new_column <- paste0("o_to_", prefix)
-  
+
   df <- df %>%
     mutate(
       # compute distances
-      dis_x = {{name_x}} - x, 
+      dis_x = {{name_x}} - x,
       dis_y = {{name_y}} - y,
-      
+
       # get atan2 in degrees
       tmp = atan2(dis_y, dis_x) * (180 / pi),
-      
+
       # set clockwise (360 - tmp) with 0 on top instead of east (+ 90)
       # https://math.stackexchange.com/questions/707673/find-angle-in-degrees-from-one-point-to-another-in-2d-space
       tmp = (360 - tmp) + 90,
-      
+
       # make sure 0 to 360
-      tmp = case_when(tmp < 0 ~ tmp + 360, 
+      tmp = case_when(tmp < 0 ~ tmp + 360,
                       tmp > 360 ~ tmp - 360 ,
                       TRUE ~ tmp),
-      
+
       # difference in angles
       diff = abs(o - tmp),
-      
+
       # angle to qb
       !!new_column := pmin(360 - diff, diff)
     )
-  
+
   return(df)
-  
+
 }
 
 # restrict frame range by events
-cut_plays <- function(df, 
+#' @export
+cut_plays <- function(df,
 
   # cut off anything that happens after this event
-  end_events = c("pass_forward", "qb_sack", "qb_strip_sack", "qb_spike", "tackle", "pass_shovel"), 
+  end_events = c("pass_forward", "qb_sack", "qb_strip_sack", "qb_spike", "tackle", "pass_shovel"),
   # keep this many frames after the end event
   time_after_event = 0,
   # remove plays with throws before this frame
   throw_frame = 25) {
-  
+
   # default truncates data at pass
   if (!is.null(end_events)) {
-    
+
     mins <- df %>%
       arrange(game_id, play_id, frame_id) %>%
       group_by(game_id, play_id) %>%
@@ -305,30 +247,138 @@ cut_plays <- function(df,
       # if throw happens on frame 36 and user wants 5 frames, keep 36 - 40
       mutate(end_frame = frame_id + time_after_event - 1) %>%
       select(game_id, play_id, end_frame)
-    
+
     df <- df %>%
       left_join(mins, by = c("game_id", "play_id")) %>%
       filter(frame_id <= end_frame)
-    
-    
+
+
   }
-  
+
   # if the play ends before throw_frame, throw out the play
   # frame 25 is 1.5 seconds after snap
   if (!is.null(throw_frame)) {
-    
+
     df <- df %>%
       arrange(game_id, play_id, frame_id) %>%
       group_by(game_id, play_id) %>%
       mutate(max_frame = max(frame_id)) %>%
       filter(max_frame >= throw_frame) %>%
       ungroup()
-    
+
   }
-  
+
   return(df)
-  
+
 }
 
+
+
+#' @export
+prepare_bdb_week <- function(
+  week,
+  dir = "../nfl-big-data-bowl-2021/input",
+  trim_frame = 25,
+  frames_after_throw = 10,
+  keep_frames = NULL,
+  drop_positions = c("QB")
+  ) {
+  df <- read_csv(glue::glue("{dir}/week{week}.csv")) %>%
+
+    # do all the cleaning
+    clean_and_rotate() %>%
+    # stop plays at pass forward
+    # and remove short (< 1 seconds) plays
+    cut_plays(
+      # if throw happens before this frame (1.5 seconds after snap), discard
+      throw_frame = trim_frame,
+      # keep this many frames after the throw
+      time_after_event = frames_after_throw
+    )
+
+  # get qb location
+  # first, throw out plays with 2 qbs
+  n_qbs <- df %>%
+    filter(position == "QB") %>%
+    group_by(game_id, play_id, frame_id) %>%
+    summarize(qbs = n()) %>%
+    group_by(game_id, play_id) %>%
+    summarise(qbs = max(qbs)) %>%
+    filter(qbs == 1) %>%
+    ungroup()
+
+  # now get the location of the QB
+  qbs <- df %>%
+    filter(position == "QB") %>%
+    dplyr::select(
+      game_id,
+      play_id,
+      frame_id,
+      qb_x = x,
+      qb_y = y
+    ) %>%
+    inner_join(n_qbs, by = c("game_id", "play_id")) %>%
+    select(-qbs)
+
+  # add qb location
+  df <- df %>%
+    # inner_join(labels, by = c("game_id", "play_id")) %>%
+    left_join(qbs, by = c("game_id", "play_id", "frame_id")) %>%
+    compute_o_diff("qb") %>%
+    # scale 0 to 1
+    mutate(o_to_qb = o_to_qb / 180) %>%
+    dplyr::filter(
+      position != drop_positions,
+      !is.na(position),
+      !is.na(o_to_qb)
+    ) %>%
+    dplyr::select(
+      game_id,
+      play_id,
+      week,
+      frame_id,
+      nfl_id,
+      play,
+      defense,
+      # coverage,
+      x,
+      y,
+      s_x,
+      s_y,
+      a_x,
+      a_y,
+      o_to_qb,
+      los_x,
+      dist_from_los
+    ) %>%
+    # this slice is probably not necessary anymore after getting rid of plays
+    # with 2 qbs
+    group_by(game_id, play_id, frame_id, nfl_id) %>%
+    dplyr::slice(1) %>%
+    # for getting rid of plays without any defense or offense players
+    group_by(game_id, play_id, frame_id) %>%
+    mutate(
+      n_defenders = sum(defense),
+      n_offense = sum(1 - defense)
+    ) %>%
+    ungroup() %>%
+    filter(
+      n_defenders > 2 & n_offense > 2
+    )
+
+  if (!is.null(keep_frames)) {
+    df <- df %>%
+      filter(
+        frame_id %in% keep_frames
+      )
+  }
+
+  df %>%
+    select(-game_id, -play_id, -n_defenders, -n_offense) %>%
+    arrange(
+      play, frame_id, defense, nfl_id
+    )
+
+}
 
 
--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -1,14 +1,9 @@
 
 
-nfl_field <- sportyR::geom_football(
-  'nfl', 
-  # the CC at the end gives the field lower alpha
-  grass_color = "#196f0cCC"
-)
-
+#' @export
 plot_play <- function(
-  df_track, 
-  orientation = TRUE, 
+  df_track,
+  orientation = TRUE,
   dot_size = 6,
   segment_length = 2.5,
   segment_size = 1.5,
@@ -19,23 +14,23 @@ plot_play <- function(
   animated_res = 200,
   frame = NULL
   ) {
-  
+
   caption <- glue::glue("{df_track$nflfastr_game_id[1]} {df_track$down[1]}&{df_track$ydstogo[1]}: Q{df_track$qtr[1]} {df_track$desc[1]}")
-  
+
   if (!is.null(frame)) {
-    
+
     df_track <- df_track %>% filter(frame_id == frame)
-    
+
   }
-  
+
   fig <- nfl_field +
     # dots
-    geom_point(data = df_track, aes(x, y), 
-               color = df_track$team_color, 
+    geom_point(data = df_track, aes(x, y),
+               color = df_track$team_color,
                shape = ifelse(
                  df_track$team_name == "football" | df_track$defense == 1,
                  19, 1
-               ), 
+               ),
                size = dot_size
     ) +
     labs(
@@ -46,9 +41,9 @@ plot_play <- function(
       plot.margin = margin(.1, 0, .5, 0, "cm"),
       plot.caption = element_text(size = 8)
     )
-  
+
   if (orientation == TRUE & "o" %in% names(df_track)) {
-    
+
     fig <- fig +
       # orientation lines
       geom_segment(
@@ -56,57 +51,44 @@ plot_play <- function(
         aes(x, y, xend = x + segment_length * o_x, yend = y + segment_length * o_y),
         color = df_track$team_color, size = segment_size
       )
-    
+
   }
-  
+
   if (numbers) {
-    
+
     fig <- fig +
       geom_text(
         data = df_track,
         mapping = aes(x = x, y = y, label = jersey_number),
         colour = ifelse(df_track$defense == 1, df_track$team_color2, "white"),
         size = 2
-      ) 
-    
+      )
+
   }
-  
+
   if (animated) {
-    
+
     # if (animated_output == "mp4") {
     #   renderer <- gganimate::gifski_renderer()
     # } else {
     #   renderer <- gganimate::av_renderer()
     # }
-    # 
+    #
     fig <- fig +
       gganimate::transition_time(df_track$frame_id)
-    
+
     fig <- gganimate::animate(
-     fig, 
+     fig,
      # renderer = renderer,
-     height = animated_h, width = animated_w, units = "in", 
+     height = animated_h, width = animated_w, units = "in",
      res = animated_res,
      nframes = n_distinct(df_track$frame_id),
      start_pause = 6,
      end_pause = 4
     )
-    
+
   }
-  
+
   return(fig)
-  
-}
 
-animate_play <- function(
-  df_track,
-  height = 4,
-  width = 8,
-  res = 200,
-  type = "gif"
-  ) {
-  
-  
-  
 }
-
